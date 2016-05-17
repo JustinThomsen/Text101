0b45413149252dd43c0dc7b2fa5430764c371f98
Justin <justinrobertthomsen@gmail.com>
2016-05-16 20:46:55 -0500

    Hole in neck tests and state

--- Assets/HoleInNeck.cs
+++ Assets/HoleInNeck.cs
@@ -17,11 +17,14 @@ namespace Thomsen.FPMITAPrison
 		#region State implementation
 		public string printOptions ()
 		{
-			throw new NotImplementedException ();
+			return "You die from a giant hole in your neck. \n\nPress Escape to start over.";
 		}
 		public State handleInput (KeyCode code)
 		{
-			throw new NotImplementedException ();
+			if (code == KeyCode.Escape) {
+				return new StateStart();
+			} else
+				return new HoleInNeck();
 		}
 		#endregion
 	}
